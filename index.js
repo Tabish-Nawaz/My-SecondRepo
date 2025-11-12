900f8054e1d57666d3577225b64feb2f35f72b15
Tabish-Nawaz <tabishnawaz311@gmail.com>
2025-11-12 21:42:14 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,83 +1,108 @@
-let Task_Array = [];
+let ArrayOfTask = [];
 
-function ShowMenu() {
+function ShowUserMenu() {
     return prompt(
         "TO-DO LIST MENU\n" +
         "1 Add Task\n" +
         "2 Update Task\n" +
         "3 Delete Task\n" +
         "4 Show Task\n" +
-        "5 Exit\n\n" +
-        "Enter the number (1 to 5)" 
+        "5 Switch Task\n" +
+        "6 Exit\n\n" +
+        "Enter the number (1 to 5)"
     );
 }
 
+function CollectIndex(Val) {
+    let index = prompt("Select the Task Number");
+    return index = parseInt(index) -1;
+}
+
 function AddTask() {
-    let UserTask = prompt("Enter the Task");
-    if(Task_Array === "") {
-        alert("Empty Task are not Allowed");
-    }else {
-        Task_Array.push(UserTask);
-        alert("Task Added");
+    let Task = prompt("Enter your task");
+    if(Task === ""){
+        alert("Error Empty Task Are not allowed");
+    }else{
+    ArrayOfTask.push(Task);
     }
 }
 
-function updateTask() {
-    if(Task_Array.length === 0){
-        alert("Empty Task");
+function UpdateTask() {
+    if(ArrayOfTask.length === 0){
+        alert("No task to edit & update");
         return;
     }
-    let index = prompt("Enter the number of the task");
-    index = parseInt(index) - 1;
+    let index = CollectIndex();
 
-    if (index >= 0 && index < Task_Array.length) {
-        let NewTask = prompt("Enter your New Task");
-        Task_Array[index] = NewTask;
-        alert("Task Updated");
-    } else {
-        alert("Invalid the task Number");
-    }
-}
-
-function DeleteTask() {
-    if(Task_Array.length === 0) {
-        alert("Empty Task");
-        return;
-    }
-    let index = prompt("Enter the task number");
-    index = parseInt(index) -1;
-
-    if(index >= 0 && index < Task_Array.length){
-        let Delete = Task_Array.splice(index , 1);
-        alert(`Task Deleted: ${Delete}`);
-    }else {
+    if(index >= 0 && index < ArrayOfTask.length){
+    let Task = prompt("Enter the task");
+    ArrayOfTask[index] = Task;
+    alert(`Updated => ${Task}`);
+    }else{
         alert("Error");
     }
 }
 
-function ShowTask() {
-    if(Task_Array.length === 0){
-        alert("Empty Tasks");
+function DeleteTask() {
+    if(ArrayOfTask.length === 0){
+        alert("Error");
+        return;
+    }
+
+    let index = CollectIndex();
+    if(index >= 0 && index < ArrayOfTask.length){
+        ArrayOfTask.splice(index, 1);
+        alert("Task Deleted");
     }else {
-        let list = "Your Task: \n";
-        Task_Array.forEach((task,i) =>{
-            list += `${i + 1}. ${task}\n `
-        });
-        alert(list);
+        alert("No task to deleted");
     }
 }
+
+function ShowTask() {
+    if(ArrayOfTask.length === 0){
+        alert("Nothing here add the task.");
+        return;
+    }else {
+        let list = "Your Task\n";
+        ArrayOfTask.forEach((tasks, i) => {
+            list += `${i+1}. ${tasks}\n`;
+        }); 
+        alert(list);
+   }
+}
+
+function SwithTask() {
+    if(ArrayOfTask.length === 0){
+        alert("Nothing here add the task.");
+        return;
+    }else if(ArrayOfTask.length < 2) {
+        alert("Switch are not possible");
+        
+    }
+    let Task_index1 = prompt("Select the Number of first task to you want to switch");
+    Task_index1 = parseInt(Task_index1) -1;
+    let Task_index2 = prompt("Select the Number of second task to you want to switch");
+    Task_index2 = parseInt(Task_index2) -1;
+    
+    let Swap = ArrayOfTask[Task_index1];
+    ArrayOfTask[Task_index1] = ArrayOfTask[Task_index2]
+    ArrayOfTask[Task_index2] = Swap;
+}
+
 
 while(true) {
-    let choice = ShowMenu();
-
+    let choice = ShowUserMenu();
     if(choice === '1') AddTask();
-    else if(choice === '2') updateTask();
+    else if(choice === '2') UpdateTask();
     else if(choice === '3') DeleteTask();
     else if(choice === '4') ShowTask();
-    else if(choice === '5') {
-        alert("Exiting....");
+    else if(choice === '5') SwithTask();
+    else if(choice === '6') {
+        alert("Exiting");
         break;
     }else{
-        alert("Invalid Choise");
+        alert("Please Invalid Number add the number");
     }
 }
+
+
